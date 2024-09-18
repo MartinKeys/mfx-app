cd26a193d838a9eb90bd4e1b52058e523357b089
vmk <kupka.vojtech@gmail.com>
2024-09-18 21:09:59 +0200

    + cleanup: empty layout deleted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { FullComponent } from './layouts/full/full.component';
+import { FullComponent } from './layouts/full.component';
 
 export const routes: Routes = [
   {
