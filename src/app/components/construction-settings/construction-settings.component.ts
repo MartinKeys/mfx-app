52af71f8ff5ea71eec32568488c756e3ca5d5dfa
vmk <kupka.vojtech@gmail.com>
2024-09-19 22:33:34 +0200

    + settings template update

--- src/app/components/construction-settings/construction-settings.component.ts
+++ src/app/components/construction-settings/construction-settings.component.ts
@@ -1,6 +1,6 @@
-import {Component, inject} from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { MaterialModule } from 'src/app/material.module';
-import {FormBuilder, Validators, NonNullableFormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, NonNullableFormBuilder } from '@angular/forms';
 import { NgFor } from '@angular/common';
 
 @Component({
@@ -17,18 +17,12 @@ export class ConstructionSettingsComponent {
 
   private _formBuilder = inject(FormBuilder) as NonNullableFormBuilder;
 
-  // firstFormGroup = this._formBuilder.group({
-  //   constructionType: ['', Validators.required],
-  // });
   firstFormGroup = this._formBuilder.group({
     constructionType: this._formBuilder.control<number>(0, Validators.required),
   });
   secondFormGroup = this._formBuilder.group({
     profileLength: ['', Validators.required],
   });
-  // thirdFormGroup = this._formBuilder.group({
-  //   loadType: ['', Validators.required],
-  // });
   thirdFormGroup = this._formBuilder.group({
     loadType: this._formBuilder.control<number>(2, Validators.required),
   });
@@ -77,84 +71,5 @@ export class ConstructionSettingsComponent {
   setLoadType(value: number) {
     this.thirdFormGroup.controls['loadType'].setValue(value);
   }
-
-
-
-
-
-  // private _formBuilder = inject(FormBuilder);
-
-  // firstFormGroup = this._formBuilder.group({
-  //   constructionType: ['', Validators.required],
-  // });
-  // secondFormGroup = this._formBuilder.group({
-  //   profileLength: ['', Validators.required],
-  // });
-  // thirdFormGroup = this._formBuilder.group({
-  //   loadType: ['', Validators.required],
-  // });
-  // fourthFormGroup = this._formBuilder.group({
-  //   profileType: ['', Validators.required],
-  // });
-
-  // // Variables to hold the parameters
-  // constructionType: number;
-  // profileLength: number;
-  // loadType: number;
-  // profileType: string;
-
-  // ngOnInit() {
-  //   // Subscribe to value changes and log parameters
-  //   this.firstFormGroup.get('constructionType')!.valueChanges.subscribe((value) => {
-  //     this.constructionType = value;
-  //     this.logParameters();
-  //   });
-  //   this.secondFormGroup.get('profileLength')!.valueChanges.subscribe((value) => {
-  //     this.profileLength = value;
-  //     this.logParameters();
-  //   });
-  //   this.thirdFormGroup.get('loadType')!.valueChanges.subscribe((value) => {
-  //     this.loadType = value;
-  //     this.logParameters();
-  //   });
-  //   this.fourthFormGroup.get('profileType')!.valueChanges.subscribe((value) => {
-  //     this.profileType = value;
-  //     this.logParameters();
-  //   });
-  // }
-
-  // logParameters() {
-  //   console.log('Construction Type:', this.constructionType);
-  //   console.log('Profile Length:', this.profileLength);
-  //   console.log('Load Type:', this.loadType);
-  //   console.log('Profile Type:', this.profileType);
-  // }
-
-  // // Methods to set values when icons are clicked
-  // setConstructionType(value: number) {
-  //   this.firstFormGroup.controls['constructionType'].setValue(value);
-  // }
-
-  // setLoadType(value: number) {
-  //   this.thirdFormGroup.controls['loadType'].setValue(value);
-  // }
-
-
-
-
-
-
-
-
-
-
-  // private _formBuilder = inject(FormBuilder);
-
-  // firstFormGroup = this._formBuilder.group({
-  //   firstCtrl: ['', Validators.required],
-  // });
-  // secondFormGroup = this._formBuilder.group({
-  //   secondCtrl: ['', Validators.required],
-  // });
 
 }
