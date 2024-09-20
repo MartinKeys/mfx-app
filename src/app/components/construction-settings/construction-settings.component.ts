abec81c0e0eff25bdb513c02712f180bfd5a9501
vmk <kupka.vojtech@gmail.com>
2024-09-20 16:14:41 +0200

    + mat-stepper binded with url

--- src/app/components/construction-settings/construction-settings.component.ts
+++ src/app/components/construction-settings/construction-settings.component.ts
@@ -1,112 +1,149 @@
-import { Component, inject, ViewChild } from '@angular/core';
-import { MaterialModule } from 'src/app/material.module';
-import { FormBuilder, Validators, NonNullableFormBuilder } from '@angular/forms';
-import { NgFor } from '@angular/common';
+import { Component, ViewChild, OnInit, inject } from '@angular/core';
+import { FormBuilder, Validators, NonNullableFormBuilder, FormControl } from '@angular/forms';
 import { MatStepper } from '@angular/material/stepper';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MaterialModule } from 'src/app/material.module'; // Ensure this module includes necessary Angular Material modules
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-construction-settings',
   standalone: true,
-  imports: [
-    MaterialModule,
-    NgFor
-  ],
+  imports: [MaterialModule, NgFor],
   templateUrl: './construction-settings.component.html',
-  styleUrl: './construction-settings.component.scss'
+  styleUrls: ['./construction-settings.component.scss'],
 })
-export class ConstructionSettingsComponent {
-
+export class ConstructionSettingsComponent implements OnInit {
   @ViewChild('stepper') private myStepper: MatStepper;
 
+  // Inject services
   private _formBuilder = inject(FormBuilder) as NonNullableFormBuilder;
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
+  private initialLoad = true;
+  constructionType: number | null = null;
+  profileLength: number | null = null;
+  loadType: number | null = null;
+  profileType: string | null = null;
+
+  // Define form groups with explicit control types
   firstFormGroup = this._formBuilder.group({
-    constructionType: this._formBuilder.control<number>(0, Validators.required),
+    constructionType: this._formBuilder.control<number | null>(null, Validators.required),
   });
   secondFormGroup = this._formBuilder.group({
-    profileLength: ['', Validators.required],
+    profileLength: this._formBuilder.control<number | null>(null, Validators.required),
   });
   thirdFormGroup = this._formBuilder.group({
-    loadType: this._formBuilder.control<number>(2, Validators.required),
+    loadType: this._formBuilder.control<number | null>(null, Validators.required),
   });
   fourthFormGroup = this._formBuilder.group({
-    profileType: ['', Validators.required],
+    profileType: this._formBuilder.control<string | null>(null, Validators.required),
   });
 
-  // Variables to hold the parameters
-  constructionType: number;
-  profileLength: number;
-  loadType: number;
-  profileType: string;
-
   ngOnInit() {
-    // Subscribe to value changes and log parameters using non-null assertion operator
+    // Subscribe to value changes and update parameters
     this.firstFormGroup.get('constructionType')!.valueChanges.subscribe((value) => {
       this.constructionType = Number(value);
+      if (!this.initialLoad) {
+        this.updateQueryParams();
+        this.setStepperIndex();
+      }
     });
     this.secondFormGroup.get('profileLength')!.valueChanges.subscribe((value) => {
       this.profileLength = Number(value);
+      if (!this.initialLoad) {
+        this.updateQueryParams();
+        this.setStepperIndex();
+      }
     });
     this.thirdFormGroup.get('loadType')!.valueChanges.subscribe((value) => {
       this.loadType = Number(value);
+      if (!this.initialLoad) {
+        this.updateQueryParams();
+        this.setStepperIndex();
+      }
     });
     this.fourthFormGroup.get('profileType')!.valueChanges.subscribe((value) => {
       this.profileType = value;
-    });
-  }
-
-  ngAfterViewInit() {
-    // Subscribe to value changes and advance the stepper
-    this.firstFormGroup.get('constructionType')!.valueChanges.subscribe((value) => {
-      this.constructionType = Number(value);
-      this.logParameters();
-
-      if (this.firstFormGroup.valid) {
-        this.myStepper.next();
+      if (!this.initialLoad) {
+        this.updateQueryParams();
+        this.setStepperIndex();
       }
     });
 
-    this.secondFormGroup.get('profileLength')!.valueChanges.subscribe((value) => {
-      this.profileLength = Number(value);
-      this.logParameters();
-
-      // Defer the execution to allow the form validity to update
+    // Read query parameters from URL and set form controls accordingly
+    this.route.queryParams.subscribe((params) => {
+      // console.log('Received query parameters:', params); // Debugging
+      // Set form controls if parameters exist
+      if (params['constructionType'] !== undefined) {
+        this.firstFormGroup.controls['constructionType'].setValue(Number(params['constructionType']));
+        this.constructionType = Number(params['constructionType']);
+      }
+      if (params['profileLength'] !== undefined) {
+        this.secondFormGroup.controls['profileLength'].setValue(Number(params['profileLength']));
+        this.profileLength = Number(params['profileLength']);
+      }
+      if (params['loadType'] !== undefined) {
+        this.thirdFormGroup.controls['loadType'].setValue(Number(params['loadType']));
+        this.loadType = Number(params['loadType']);
+      }
+      if (params['profileType'] !== undefined) {
+        this.fourthFormGroup.controls['profileType'].setValue(params['profileType']);
+        this.profileType = params['profileType'];
+      }
+      // Force validity update
+      this.firstFormGroup.updateValueAndValidity({ onlySelf: true });
+      this.secondFormGroup.updateValueAndValidity({ onlySelf: true });
+      this.thirdFormGroup.updateValueAndValidity({ onlySelf: true });
+      this.fourthFormGroup.updateValueAndValidity({ onlySelf: true });
+      // After setting initial values, reset the flag
+      this.initialLoad = false;
+      // Defer the call to setStepperIndex to ensure form validity has been updated
       setTimeout(() => {
-        if (this.secondFormGroup.valid) {
-          this.myStepper.next();
-        }
+        this.setStepperIndex();
       });
     });
-
-    this.thirdFormGroup.get('loadType')!.valueChanges.subscribe((value) => {
-      this.loadType = Number(value);
-      this.logParameters();
-
-      if (this.thirdFormGroup.valid) {
-        this.myStepper.next();
-      }
-    });
-
-    this.fourthFormGroup.get('profileType')!.valueChanges.subscribe((value) => {
-      this.profileType = value;
-      this.logParameters();
-    });
   }
 
-  logParameters() {
-    console.log('Construction Type:', this.constructionType);
-    console.log('Profile Length:', this.profileLength);
-    console.log('Load Type:', this.loadType);
-    console.log('Profile Type:', this.profileType);
+  setStepperIndex() {
+    let highestValidStep = 0;
+    if (this.firstFormGroup.valid) highestValidStep = 1;
+    if (this.secondFormGroup.valid) highestValidStep = 2;
+    if (this.thirdFormGroup.valid) highestValidStep = 3;
+    if (this.fourthFormGroup.valid) highestValidStep = 3; // Last index is 3 (0-based)
+    // Mark previous steps as completed
+    this.myStepper.steps.forEach((step, index) => {
+      step.completed = index < highestValidStep;
+    });
+    // Set the stepper to the highest valid step
+    this.myStepper.selectedIndex = highestValidStep;
+  }
+
+  updateQueryParams() {
+    const queryParams: any = {
+      constructionType: this.constructionType,
+      profileLength: this.profileLength,
+      loadType: this.loadType,
+      profileType: this.profileType,
+    };
+    // Remove undefined or null parameters
+    Object.keys(queryParams).forEach(
+      (key) =>
+        (queryParams[key] === undefined || queryParams[key] === null || queryParams[key] === '') &&
+        delete queryParams[key]
+    );
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: queryParams,
+      queryParamsHandling: 'merge',
+    });
   }
 
   // Methods to set values when icons are clicked
   setConstructionType(value: number) {
     this.firstFormGroup.controls['constructionType'].setValue(value);
   }
-
   setLoadType(value: number) {
     this.thirdFormGroup.controls['loadType'].setValue(value);
   }
-
 }
