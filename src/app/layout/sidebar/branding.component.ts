ac819d9c159e49be2f94c2df38ffc7b6fad81816
vmk <kupka.vojtech@gmail.com>
2024-09-24 16:31:57 +0200

    + assets structure updated

--- src/app/layout/sidebar/branding.component.ts
+++ src/app/layout/sidebar/branding.component.ts
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
     <div class="branding">
       <a [routerLink]="['/']">
         <img
-          src="./assets/images/logos/logo.svg"
+          src="./assets/graphics/logos/logo.svg"
           class="align-middle m-2"
           alt="logo"
           width="180" 
